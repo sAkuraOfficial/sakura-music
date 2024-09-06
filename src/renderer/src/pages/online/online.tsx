0f5063e352760f1d1d7d983a5ab0bfd2305f9985
sAkuraOfficial <mchaohao159@outlook.com>
2024-09-06 18:12:35 +0800

    ✨ 删除一些杂项文件

--- src/renderer/src/pages/online/online.tsx
+++ src/renderer/src/pages/online/online.tsx
@@ -1,36 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import MusicCard from '../../components/MusicCard/MusicCard'
+// import React, { useEffect, useState } from 'react'
+// import MusicCard from '../../components/MusicCard/MusicCard'
 import './Online.css'
-import zj from './zj.png'
 
 const Online: React.FC = () => {
-  const [musicData, setMusicData] = useState([])
-  const [isPlaying, setIsPlaying] = useState(false)
+  // const [musicData, setMusicData] = useState([])
+  // const [isPlaying, setIsPlaying] = useState(false)
+  //
+  // useEffect(() => {
+  //   fetch('/api/music')
+  //     .then((response) => response.json())
+  //     .then((data) => setMusicData(data))
+  //     .catch((error) => console.error('Error fetching music data:', error))
+  // }, [])
 
-  useEffect(() => {
-    fetch('/api/music')
-      .then((response) => response.json())
-      .then((data) => setMusicData(data))
-      .catch((error) => console.error('Error fetching music data:', error))
-  }, [])
-
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying)
-  }
+  // const handlePlayPause = () => {
+  //   setIsPlaying(!isPlaying)
+  // }
 
   return (
     <div className={'Page-Online'}>
-      {musicData.map((music) => (
-        <MusicCard
-          key={music.id}
-          title={music.title}
-          artist={music.artist}
-          img={zj}
-          isPlaying={isPlaying}
-          onPlayPause={handlePlayPause}
-        />
-      ))}
+      {/*{musicData.map((music) => (*/}
+      {/*  <MusicCard*/}
+      {/*    key={music.id}*/}
+      {/*    title={music.title}*/}
+      {/*    artist={music.artist}*/}
+      {/*    img={zj}*/}
+      {/*    isPlaying={isPlaying}*/}
+      {/*    onPlayPause={handlePlayPause}*/}
+      {/*  />*/}
+      {/*))}*/}
     </div>
   )
 }
